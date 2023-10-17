d693a65c775a1d810619e2110d764c4d6377191b
saadraj4 <sr201040@gmail.com>
2023-10-17 10:41:37 +0500

    Initial table setup with static data

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Table from './components/Table'
+import { sampleData } from './components/Data'
+import React from 'react'
+
+
+function App() {
+
+
+
+
+  return (
+    <>
+    <div>
+      
+       <Table />
+    </div>
+
+    </>
+  )
+}
+
+export default App
